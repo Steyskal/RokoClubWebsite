d527c420790a9ad0601dc336376ef34586c89995
Toni Steyskal <toni.steyskal@gmail.com>
2015-09-08 15:33:09 +0200

    Basic Template

---
+++ Roko Club - Website/RokoClub/event.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Bootstrap 101 Template</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap-cyborg.css" rel="stylesheet">
+
+        <link href="css/sticky-footer.css" rel="stylesheet">
+        <link href="css/roko-style.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <div class="row roko-body">
+            <div class="col-md-4">
+                <h1>BECKS LOGO</h1>
+            </div>
+
+            <div class="col-md-8">
+                <div class="col-md-12">
+                    <h1>EVENT POSTER</h1>
+                </div>
+
+                <div class="col-md-12">
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in nisi urna. In ipsum nunc, porta vel ultricies non, tristique vel nisl.</h5>
+                    <p class="text-muted text-justify">Nulla facilisi. Aliquam gravida odio sed turpis fermentum, eget hendrerit magna interdum. Vestibulum imperdiet euismod scelerisque. Phasellus in dapibus metus. Proin ornare lobortis erat, tempus vulputate tortor pellentesque vel. In ac tristique mi, at viverra mauris.</p>
+                    <h6 class="text-muted">DATUM</h6>
+                    <h6 class="text-muted">ROKO Club&Lounge</h6>
+                </div>
+
+                <div class="col-md-12">
+                    <a href="index.php"><h3 class="glyphicon glyphicon-menu-left">BACK</h3></a>
+                </div>
+
+                <div class="col-md-offset-3 col-md-6">
+                    <h1>ROKO LOGO</h1>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">DD3D LOGO + INFO</p>
+        </div>
+    </footer>
+
+</html>
---
+++ Roko Club - Website/RokoClub/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Bootstrap 101 Template</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap-cyborg.css" rel="stylesheet">
+
+        <link href="css/sticky-footer.css" rel="stylesheet">
+        <link href="css/roko-style.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <div class="row roko-body">
+            <div class="col-md-4">
+                <h1>BECKS LOGO</h1>
+            </div>
+
+            <div class="col-md-8">
+                <a href="event.php">
+                    <div class="col-md-6">
+                        <div class="panel panel-success">
+                            <div class="panel-body">
+                                EVENT 1
+                            </div>
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Klikni za event info!</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <div class="col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-body">
+                            EVENT 2
+                        </div>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Klikni za event info!</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-body">
+                            EVENT 3
+                        </div>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Klikni za event info!</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-body">
+                            EVENT 4
+                        </div>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Klikni za event info!</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-body">
+                            EVENT 5
+                        </div>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Klikni za event info!</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="panel panel-success">
+                        <div class="panel-body">
+                            EVENT 6
+                        </div>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Klikni za event info!</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-offset-3 col-md-6">
+                    <h1>ROKO LOGO</h1>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">DD3D LOGO + INFO</p>
+        </div>
+    </footer>
+
+</html>
